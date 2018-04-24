10b6fc5d76f8d25077b751bcc5a26a501ce01f67
finppp <finlaypercy@hotmail.com>
2018-04-24 15:31:11 +0100

    added start of portfolio

--- src/components/portfolio/PortfolioSlider.js
+++ src/components/portfolio/PortfolioSlider.js
@@ -3,18 +3,32 @@ import styled from 'styled-components';
 
 class PortfolioSlider extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPorfolioItem: 2
+    };
+  }
+
+  changePortfolioItem = (newItem) => {
+    console.log("newItem");
+    this.setState({
+      currentPorfolioItem: newItem
+    })
+  }
+
   render() {
     return (
       <Container>
-        <Picker>
-          <Slide/>
-          <Slide/>
-          <Slide/>
-          <Slide/>
-          <Slide/>
-          <Slide/>
-          <Slide/>
-        </Picker>
+        <PickerSection>
+          <Pick active={this.state.currentPorfolioItem == 1} onClick={() => this.changePortfolioItem(1)} title="RowSquad"/>
+          <Pick active={this.state.currentPorfolioItem == 2} onClick={() => this.changePortfolioItem(2)} title="MyBoathouse"/>
+          <Pick active={this.state.currentPorfolioItem == 3} onClick={() => this.changePortfolioItem(3)} title="Paddywack Prints"/>
+          <Pick active={this.state.currentPorfolioItem == 4} onClick={() => this.changePortfolioItem(4)} title="The Cake Valley"/>
+          <Pick active={this.state.currentPorfolioItem == 5} onClick={() => this.changePortfolioItem(5)} title="Telscombe Village"/>
+          <Pick active={this.state.currentPorfolioItem == 6} onClick={() => this.changePortfolioItem(6)} title="Folded Group"/>
+          <Pick active={this.state.currentPorfolioItem == 7} onClick={() => this.changePortfolioItem(7)} title="tshirt.co.uk"/>
+        </PickerSection>
         <ViewSlide/>
       </Container>
     );
@@ -30,32 +44,77 @@ const Container = styled.div`
   background-color: green;
 `
 
-const Picker = styled.div`
-  display: inline-block;
+const PickerSection = styled.div`
+  float: left;
   width: 30%;
   height: 100%;
-  background-color: green;
+  background-color: orange;
 `
 
-
-
-
-
-class Slide extends Component {
+class Pick extends Component {
   render() {
     return (
-      <SlideContainer>
-        SLIDE
-      </SlideContainer>
+      <PickContainer onClick={this.props.onClick} active={this.props.active}>
+        <SlideContainer className="slides">
+        <p>{this.props.title}</p>
+        </SlideContainer>
+        <ArrowContainer>
+          <i className="fas fa-arrow-right"></i>
+        </ArrowContainer>
+      </PickContainer>
     )
   }
 }
 
-const SlideContainer = styled.div`
+
+const PickContainer = styled.div`
+  position: relative;
   width: 100%;
-  height: calc(100%/8);
-  display: inline-block;
+  height: 12.5%;
+  background-color: rgb(150, 182, 194);
+  cursor: pointer;
+  p {
+    text-decoration: ${props => props.active ? 'underline' : 'none'};
+    color: ${props => props.active ? 'white' : 'black'};
+    margin: 0;
+    font-size: 2em;
+  }
+  &:hover {
+    p, i {
+      color: white;
+    }
+    > .slides {
+      width: 80%;
+    }
+  }
 `
+
+const SlideContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: width 1s ease;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(51, 158, 199);
+`
+
+const ArrowContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  float: right;
+  width: 20%;
+  height: 100%;
+  background-color: rgb(51, 158, 199);
+  ${'' /* background-color: rgb(173, 233, 255); */}
+  i {
+    font-size: 2em;
+  }
+`
+
+
 
 class ViewSlide extends Component {
   render() {
